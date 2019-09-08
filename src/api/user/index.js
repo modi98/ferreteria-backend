5cb321a4bfb977c72185c296df7c5e2320ee8759
modi98 <mauricio.alvaradoh98@gmail.com>
2019-09-08 12:52:59 -0500

    Now authenticated admin users can post new users.

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -65,7 +65,7 @@ router.get('/:id',
  * @apiError 409 Email already registered.
  */
 router.post('/',
-  master(),
+  token({ required: true, roles: ['admin'] }),
   body({ email, password, name, picture, role }),
   create)
 
